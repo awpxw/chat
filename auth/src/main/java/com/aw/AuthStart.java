9bc8bd208a14c53a98b9017a59dcf9aa0bb2fac2
pxw <2286668092@qq.com>
2025-11-26 17:38:04 +0800

    代码质量门禁测试

--- auth/src/main/java/com/aw/AuthStart.java
+++ auth/src/main/java/com/aw/AuthStart.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthStart
 {
     public static void main(String[] args) {
+
         SpringApplication.run(AuthStart.class, args);
+    }
+    public class TestSonar {
+        public void bug() {
+            String s = null;
+            s.length(); // 故意空指针！Sonar 一定会报 Bug
+        }
     }
 }
