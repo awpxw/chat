c29050e4ad4a8171922f274852911bbf4c5b7a44
pxw <2286668092@qq.com>
2025-11-27 11:48:54 +0800

    代码质量门禁

---
+++ admin/src/main/java/com/aw/jwt/AdminStater.java
@@ -0,0 +1,11 @@
+package com.aw.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminStater {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminStater.class, args);
+    }
+}
